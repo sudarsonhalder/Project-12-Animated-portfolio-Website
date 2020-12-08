1785b7f91e4e19084f41c41601fd6609e51adbd6
Sudarson Halder <64659436+sudarsonhalder@users.noreply.github.com>
2020-12-08 10:17:37 +0530

    Update Navbar.jsx

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -2,36 +2,30 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 const Navbar = () => {
     return (<>
-    <div className='container-fluid nav bg'>
+    <div className='container-fluid nav_bg'>
         <div className='row'>
             <div className='col-10 mx-auto'>
-        <nav className="navbar navbar-expand-lg navbar-light  ">
-            <NavLink className="navbar-brand" to="/">Edtech</NavLink>
+        <nav className="navbar navbar-expand-lg navbar-light ">
+            <NavLink className="navbar-brand" to="/">TechGenizer</NavLink>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
 
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul className="navbar-nav mr-auto">
+                <ul className="navbar-nav ml-auto">
                     <li className="nav-item active">
                         <NavLink 
                         activeclassName="menu_active" exact className="nav-link"
                         aria-current="page" to="/">Home</NavLink>
                     </li>
                     <li className="nav-item">
-                        <NavLink  activeclassName="menu_active" className="nav-link" to="/Liveclasses">Live Classes</NavLink>
+                        <NavLink activeclassName="menu_active" className="nav-link" to="/service">Service</NavLink>
                     </li>
                     <li className="nav-item">
-                        <NavLink activeclassName="menu_active" className="nav-link" to="/Course">Courses</NavLink>
+                        <NavLink  activeclassName="menu_active" className="nav-link" to="/about">About</NavLink>
                     </li>
                     <li className="nav-item">
-                        <NavLink activeclassName="menu_active" className="nav-link" to="/Book">Books</NavLink>
-                    </li>
-                    <li className="nav-item">
-                        <NavLink activeclassName="menu_active" className="nav-link" to="/Board">Boards</NavLink>
-                    </li>
-                    <li className="nav-item">
-                        <NavLink activeclassName="menu_active" className="nav-link" to="/Exam">Exams</NavLink>
+                        <NavLink activeclassName="menu_active" className="nav-link" to="/contact">Contact</NavLink>
                     </li>
                 </ul>
             </div>
@@ -42,4 +36,4 @@ const Navbar = () => {
     </>
     )
 }
-export default Navbar;
+export default Navbar; 
